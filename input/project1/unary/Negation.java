2497738e87be1221ca871b29f86ae1007a523b1e
helpmehelpus <adrianortorres@gmail.com>
2022-12-15 23:49:39 +1030

    revert change

--- input/project1/unary/Negation.java
+++ input/project1/unary/Negation.java
@@ -1,5 +1,4 @@
 public class Negation {
     public boolean OneNegation(boolean aBoolean){return !aBoolean;}
     public boolean TwoNegation(boolean aBoolean){return !!aBoolean;}
-    public boolean reduntantTwoNegation(boolean aBoolean){return aBoolean;}
 }
